850a01d95e42e56446a2578590466f6d89bdc803
Jack Bowden <jackbowden1992@gmail.com>
2024-07-19 09:18:06 +0100

    Middleware changes

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,5 @@
+export const runtime = 'edge';
+
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
-
-export const config = {
-	// The following matcher runs middleware on all routes
-	// except static assets.
-	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
